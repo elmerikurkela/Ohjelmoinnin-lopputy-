8e4998c006ccb6f1fd51d95e6a99a51be1fad2d2
Elmeri Kurkela <elmeri.kurkela@edu.turkuamk.fi>
2025-04-02 14:17:47 +0300

    testailua

--- Kalenteri.py
+++ Kalenteri.py
@@ -76,7 +76,7 @@ def tarkastelu(vuosi, kuukausi, tapahtumat):
 
     # Haetaan tapahtumat annetulle vuodelle ja kuukaudelle
     tapahtuma_paivat = {päivä for (päivä, _) in tapahtumat.get((vuosi, kuukausi), [])}
-    tapahtuma_lista = tapahtumat.get((vuosi, kuukausi), []) # Lista päivä + kuvaus pareista
+    tapahtuma_lista = tapahtumat.get((vuosi, kuukausi), []) # Lista päivä + kuvaus pareistacd
     # Muokataan kalenterin päivät ja lisätään tähdet tapahtumapäiville
     muokattu_kuukausi = []
     for viikko in kuukauden_paivat: # Käydään läpi viikot
@@ -142,25 +142,7 @@ def poista_tapahtuma(vuosi, kuukausi, tapahtumat):
     except OSError as e:
         print(f"Virhe tiedostoon kirjoittamisessa: {e}") # Virheilmoitus muille tiedostovirheille
 
-def muokkaa_tapahtumaa():
-    muokkaus = input('Mitä haluat muokata? (Tapahtuma, Päivämäärä):')
-    infile = open(TIEDOSTO,"w") #Avataan tiedosto lukutilassa
-    if muokkaus == "Tapahtuma":
-        vanha_tapahtuma = input("Mikä tapahtuma halutaan muokata?")
-        uusi_tapahtuma = input("Mikä on uusi tapahtuma?")
-        for rivi in infile:
-            if vanha_tapahtuma in rivi:
-                rivi = rivi.replace(vanha_tapahtuma, uusi_tapahtuma)
-                print(f"Tapahtuma '{vanha_tapahtuma}' on muutettu muotoon '{uusi_tapahtuma}'.")
-    elif muokkaus == "Päivämäärä":
-        vanha_pvm = input("Mikä päivämäärä halutaan muokata? (esim. 2025 3 07)")
-        uusi_pvm = input("Mikä on uusi päivämäärä? (esim. 2025 3 07)")
-        for rivi in infile:
-            if vanha_pvm in rivi:
-                rivi = rivi.replace(vanha_pvm, uusi_pvm)
-                print(f"Päivämäärä '{vanha_pvm}' on muutettu muotoon '{uusi_pvm}'.")
-    else:
-        print("Virheellinen syöte.")
+
 
 
 
